e7db65437d0808242eeb8c9beae4ee902be7bff5
NaNeG <kolosovg02@hotmail.com>
2022-09-15 21:33:06 +0500

    Update SocketConnectionProvider.js

--- src/store/SocketConnectionProvider.js
+++ src/store/SocketConnectionProvider.js
@@ -63,7 +63,7 @@ export default function SocketConnectionProvider(props) {
 	}, [params.id, userId, users, socket, selectedMap]);
 
 	useEffect(() => {
-		const socket = new WebSocket("ws://tarkov-map-server.herokuapp.com/");
+		const socket = new WebSocket("wss://tarkov-map-server.herokuapp.com/");
 		setSocket(socket);
 		socket.onopen = () => {
 			console.log("connected");
